b9d30100c3fd925481d69158da712b4c7913028a
Demetrio <dlima.nyit@gmail.com>
2023-04-07 22:18:45 -0400

    Additional Adjustments til actual code

--- src/WorkingOn/CurrentlyEditing.jsx
+++ src/WorkingOn/CurrentlyEditing.jsx
@@ -5,7 +5,7 @@ function CurrentlyEditing() {
         <div>
             <h1>Site Is Currently Being Worked On</h1>
             <h3>Please Excuse Our Appearence</h3>
-            <h3>While we get things in order</h3>
+            <h3>While We Get Things In Order</h3>
             <image src=""/>
             <h3>Thanks!</h3>
         </div>
