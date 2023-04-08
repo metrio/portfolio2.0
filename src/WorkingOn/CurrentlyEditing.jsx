08762e0f1d1e6c045732571fbe727a4756459854
Demetrio <dlima.nyit@gmail.com>
2023-04-07 22:07:45 -0400

    Editing Site

---
+++ src/WorkingOn/CurrentlyEditing.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+
+function CurrentlyEditing() {
+    const [imageSrc, setImageSrc] = useState()
+    
+
+    return(
+        <div>
+            <h1>Site Is Currently Being Worked On</h1>
+            <h3>Please Excuse Our Appearence</h3>
+            <h3>While we get things in order</h3>
+            <image src=""/>
+            <h3>Thanks!</h3>
+        </div>
+    )
+}
+
+export default CurrentlyEditing
